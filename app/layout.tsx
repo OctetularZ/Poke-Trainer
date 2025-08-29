0e943e3945f3b270f7334d6ac6905d4956562bd2
OctetularZ <taiabayomi15@gmail.com>
2025-08-29 20:30:31 +0100

    Finally got the local font to work on with tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,22 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import Navbar from "@/components/Navbar"
+import localFont from "next/font/local"
 
 export const metadata: Metadata = {
   title: "Poke Trainer",
   description: "Train your pokemon skills",
 }
+
+const pokeFont = localFont({
+  src: [
+    {
+      path: "./fonts/Pokemon Classic.ttf",
+    },
+  ],
+  variable: "--pokefont",
+  display: "swap",
+})
 
 export default function RootLayout({
   children,
@@ -13,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={pokeFont.variable}>
       <body>
         <Navbar />
         {children}
