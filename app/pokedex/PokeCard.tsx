e42ea9e80531f2a40ee787edced23829c8dab305
OctetularZ <taiabayomi15@gmail.com>
2025-09-01 14:00:52 +0100

    Displaying ID on cards to 4sf

--- app/pokedex/PokeCard.tsx
+++ app/pokedex/PokeCard.tsx
@@ -24,7 +24,7 @@ const PokeCard = ({ id, name, sprite, types }: Pokemon) => {
             unoptimized={true}
           />
         </div>
-        <p className="text-gray-400">#{id}</p>
+        <p className="text-gray-400">#{id.toString().padStart(4, "0")}</p>
         <h1 className="text-white text-2xl pb-3 text-wrap text-center">
           {name}
         </h1>
