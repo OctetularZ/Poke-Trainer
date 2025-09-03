9e3538fcddfcffa82075c3b99e688d1edf526e74
OctetularZ <taiabayomi15@gmail.com>
2025-09-03 22:22:55 +0100

    Successfully connected to Database and have added google and github social sign ins. Working perfectly. Will add more social sign ins.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client"
 import Image from "next/image"
+import { googleSignIn } from "@/lib/auth-client"
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-white p-5">WELCOME TO POKE TRAINER!</h1>
+      <button
+        className="text-white py-1 px-3 bg-charmander-blue-400 cursor-pointer rounded"
+        onClick={googleSignIn}
+      >
+        Google
+      </button>
     </div>
   )
 }
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,3 +2,15 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     baseURL: "http://localhost:3000"
 })
+
+export const googleSignIn = async () => {
+  const data = await authClient.signIn.social({
+    provider: "google",
+  });
+};
+
+export const githubSignIn = async () => {
+    const data = await authClient.signIn.social({
+        provider: "github"
+    })
+}
