05ae632db4ef95016ba79f2e5027d58941352558
OctetularZ <taiabayomi15@gmail.com>
2025-09-04 00:41:50 +0100

    Replaced placeholder image for no pokecards with greyed out pixel great ball

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 "use client"
 import Image from "next/image"
-import { googleSignIn } from "@/lib/auth-client"
+import { githubSignIn, googleSignIn } from "@/lib/auth-client"
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-white p-5">WELCOME TO POKE TRAINER!</h1>
       <button
-        className="text-white py-1 px-3 bg-charmander-blue-400 cursor-pointer rounded"
+        className="text-white py-1 px-3 bg-red-500 cursor-pointer rounded"
         onClick={googleSignIn}
       >
         Google
+      </button>
+      <button
+        className="text-white py-1 px-3 bg-amber-600 cursor-pointer rounded"
+        onClick={githubSignIn}
+      >
+        Github
       </button>
     </div>
   )
