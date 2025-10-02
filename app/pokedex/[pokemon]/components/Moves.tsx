b8e310831b069348b92e104b6874ad4f1528a05b
OctetularZ <taiabayomi15@gmail.com>
2025-10-02 23:41:15 +0100

    Almost done with adding moves to pokemon pages, just need to make a filter.

--- app/pokedex/[pokemon]/components/Moves.tsx
+++ app/pokedex/[pokemon]/components/Moves.tsx
@@ -20,9 +20,9 @@ const Moves = ({ loading, pokemonMoves }: PokemonMoves) => {
   const moveLearnMethods = ["level-up", "machine", "tutor", "egg"]
 
   return (
-    <div className="flex flex-col gap-10 justify-center items-center w-150">
+    <div className="flex flex-col gap-10 justify-center items-center w-200">
       <Listbox value={selectedMoveFilter} onChange={setSelectedMoveFilter}>
-        <div className="relative w-full text-white">
+        <div className="relative w-full text-white text-lg">
           <ListboxButton className="flex flex-row justify-between items-center w-full px-2 py-1 rounded border border-gray-300 bg-charmander-dull-200 cursor-pointer focus:outline-none">
             {selectedMoveFilter.charAt(0).toUpperCase() +
               selectedMoveFilter.slice(1).replace("-", " ") || "All"}
@@ -58,33 +58,65 @@ const Moves = ({ loading, pokemonMoves }: PokemonMoves) => {
           </ListboxOptions>
         </div>
       </Listbox>
-      {!loading &&
-        pokemonMoves.map((move) => (
-          <div
-            key={move.id}
-            className="flex flex-row gap-10 w-full justify-center items-center"
-          >
-            <h1 className="text-white text-xl">
-              {move.name.charAt(0).toUpperCase() +
-                move.name.slice(1).replace("-", " ")}
-            </h1>
-            <h4
-              key={move.type.name}
-              className={`text-white text-2xl ${
-                typeColours[move.type.name as keyof typeof typeColours]
-              } rounded-lg px-3 shadow-md`}
-              style={{
-                filter: `drop-shadow(0 0 8px ${
-                  typeColoursHex[move.type.name as keyof typeof typeColoursHex]
-                })`,
-              }}
-            >
-              {`${move.type.name.charAt(0).toUpperCase()}${move.type.name.slice(
-                1
-              )}`}
-            </h4>
-          </div>
-        ))}
+      {!loading && (
+        <table className="w-full text-white text-center text-2xl">
+          <thead>
+            <tr className="divide-x">
+              <th className="bg-charmander-dull-200 border-t-1 border-b-1 py-2">
+                Level
+              </th>
+              <th className="bg-charmander-dull-200 border-t-1 border-b-1">
+                Move
+              </th>
+              <th className="bg-charmander-dull-200 border-t-1 border-b-1">
+                Type
+              </th>
+              <th className="bg-charmander-dull-200 border-t-1 border-b-1">
+                Category
+              </th>
+              <th className="bg-charmander-dull-200 border-t-1 border-b-1">
+                Power
+              </th>
+              <th className="bg-charmander-dull-200 border-t-1 border-b-1">
+                Accuracy
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {pokemonMoves.map((move) => (
+              <tr key={move.id} className="border-b-white border-b-1">
+                <td className="py-5">1</td>
+                <td>
+                  {move.name.charAt(0).toUpperCase() +
+                    move.name.slice(1).replace("-", " ")}
+                </td>
+                <td>
+                  <h4
+                    key={move.type.name}
+                    className={`text-white text-2xl ${
+                      typeColours[move.type.name as keyof typeof typeColours]
+                    } rounded-lg px-3 shadow-md`}
+                    style={{
+                      filter: `drop-shadow(0 0 6px ${
+                        typeColoursHex[
+                          move.type.name as keyof typeof typeColoursHex
+                        ]
+                      })`,
+                    }}
+                  >
+                    {`${move.type.name
+                      .charAt(0)
+                      .toUpperCase()}${move.type.name.slice(1)}`}
+                  </h4>
+                </td>
+                <td>{move.damage_class.name}</td>
+                <td>{move.power || "-"}</td>
+                <td>{move.accuracy || "-"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
