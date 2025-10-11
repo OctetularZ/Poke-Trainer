79eaf83228b480d6baf6d7f7b004eaa59c4ce66a
OctetularZ <taiabayomi15@gmail.com>
2025-10-11 20:30:48 +0100

    Started working on schema for storing pokemon Data (almost done). Installed a node module for running ts scripts too.

--- scripts/database/savePokemonToDB.ts
+++ scripts/database/savePokemonToDB.ts
@@ -1,2 +1,4 @@
-import prisma from "@/lib/prisma";
-import { getPokemonDetails } from '@/scraping/pokemonData'
+import prisma from "../../lib/prisma.js";
+import { getPokemonDetails } from '../../scraping/pokemonData.js'
+
+let pokemonData = getPokemonDetails();
